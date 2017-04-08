71e264e8fe4e6e0a83dca9d7498e1f7a6b9236c0
hussainHMC <hussainbadri786@gmail.com>
2017-04-08 16:26:44 +0530

    Updated Login

--- app/src/main/java/com/dev/rohitmathew/crypto/Login.java
+++ app/src/main/java/com/dev/rohitmathew/crypto/Login.java
@@ -2,12 +2,16 @@ package com.dev.rohitmathew.crypto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_login);
     }
 }
