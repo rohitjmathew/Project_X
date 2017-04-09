a04947d580efc381de400b93df542d692e6f737f
Rohit Mathew <rohitjmathew@hotmail.com>
2017-04-09 06:22:20 +0530

    Table

--- app/src/main/java/com/dev/rohitmathew/crypto/investTable.java
+++ app/src/main/java/com/dev/rohitmathew/crypto/investTable.java
@@ -5,7 +5,10 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class investTable extends AppCompatActivity {
@@ -21,7 +24,22 @@ public class investTable extends AppCompatActivity {
             JSONObject person = new JSONObject(response);
             JSONObject dat = person.getJSONObject("data");
             Log.e("tab",dat.getString("logs"));
+            JSONArray prods = dat.getJSONArray("logs");
+            if(prods != null){
+                for(int j = 0; j < prods.length();j++){
+                    JSONObject innerElem = prods.getJSONObject(j);
+                    if(innerElem != null){
+                        boolean inv_width = innerElem.getBoolean("inv_width");
+                        if(inv_width)
+                        {
+                            TextView bit = (TextView) findViewById(R.id.info_text3);
+                            bit.setText("฿ "+ (10-btc));
+                        }
+                    }
+                }
+            }
         }
         catch(Exception e){}
     }
+
 }
